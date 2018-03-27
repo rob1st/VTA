8dfd9cc55e9a057d8afccc511d9e11d0a6cd5207
ckb <ckingbailey@gmail.com>
2018-03-27 00:59:16 +0000

    make masthead logo and text smaller

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
     <main role="main">
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
-      <header class="container page-header">
-        <h1 class="page-heading"><img class="logo-md" src="vta.jpg">Silicon Valley Berryessa Extension</h1>
+      <header class="container masthead">
+        <h1 class="page-heading"><img class="masthead-logo" src="vta.jpg">Silicon Valley Berryessa Extension</h1>
         <p>This site if for use by personel working upon the SVBX project, if you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below. </p>
         <a class="btn btn-primary btn-lg" href="http://www.vta.org/News-and-Media/Connect-with-VTA/Phase-I-of-BART-Silicon-Valley-Update#.WqbH0WrwZaQ" role="button">Learn more &raquo;</a>
       </header>
