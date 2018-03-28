4eccf82fe217914f382aa32ef38716e25e6ba702
Colin <ckingbailey@gmail.com>
2018-03-28 11:40:28 -0700

    Login flow (#5)
    * correct some typos in footer
    
    * checkout development config.php
    
    * rm jumbotron from login page
    
    * use offset-4 to center login form
    
    * vertical space login elements
    
    * make masthead logo and text smaller
    
    * tighten up layout of .page-header
    
    * rebase onto master

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include('filestart.php'); ?>
         <div class="container">
           <img src="vta_logo.png">
           <h1>Silicon Valley Berryessa Extension</h1>
-          <p>This site is for use by personnel working upon the SVBX project. If you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below.</p>
+          <p>This site is for use by personnel working on the SVBX project. If you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below.</p>
           <p><a class="btn btn-primary btn-lg" href="http://www.vta.org/News-and-Media/Connect-with-VTA/Phase-I-of-BART-Silicon-Valley-Update#.WqbH0WrwZaQ" role="button">Learn more &raquo;</a></p>
         </div>
       </div>
--- login.php
+++ login.php
@@ -3,42 +3,28 @@
     <main role="main">
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <div class="container">
-            <img src="vta_logo.png">
-          <h1 class="display-3">Silicon Valley Berryessa Extension</h1>
-          <p>This site if for use by personel working upon the SVBX project, if you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below. </p>
-          <p><a class="btn btn-primary btn-lg" href="http://www.vta.org/News-and-Media/Connect-with-VTA/Phase-I-of-BART-Silicon-Valley-Update#.WqbH0WrwZaQ" role="button">Learn more &raquo;</a></p>
-        </div>
-      </div>
+      <header class="container page-header masthead">
+        <h1 class="page-title"><img class="masthead-logo" src="vta.jpg">Silicon Valley Berryessa Extension</h1>
+        <p>This site if for use by personel working upon the SVBX project, if you are not working upon the SVBX project, but would like some information, please click on the 'Learn More' button below.
+          <a href="http://www.vta.org/News-and-Media/Connect-with-VTA/Phase-I-of-BART-Silicon-Valley-Update#.WqbH0WrwZaQ" target="_blank" class="btn btn-primary btn-xs">Learn more &raquo;</a></p>
+      </header>
 
-      <div class="container">
-        <!-- Example row of columns -->
+      <div class="container login-container">
         <form action="loginSubmit.php" method="post">
-        <fieldset class="login">
-        <div class="row">
-          <div class="col-md-4">
-            <h2>Username</h2>
-            <p>
-              <input type="text" name="Username" value="" maxlength="20" />
-            </p>
-          </div>
-          <div class="col-md-4">
-            <h2>Password</h2>
-            <p>
-              <input type="password"  name="Password" value="" maxlength="20" /><br>
-            </p>
-          </div>
-          <div class="col-md-4">
-            <br />
-            <p>
-              <input type="submit" name="submit" value="Login" class="btn btn-primary btn-lg"/>
-              <a class="btn btn-primary btn-lg" href="ForgotPassword.php" role="button">Forgot Password</a>
-            </p>
-          </div>
-        </div>
-        </fieldset>
+          <fieldset class="login">
+            <div class="row">
+              <div class="col-md-4 offset-md-4">
+                <h2>Username</h2>
+                <input type="text" name="Username" value="" maxlength="20" class="login-field" />
+                <h2>Password</h2>
+                <input type="password"  name="Password" value="" maxlength="20" class="login-field" />
+              <input type="submit" name="submit" value="Login" class="btn btn-primary btn-lg login-btn"/>
+              <a href="ForgotPassword.php" class="forgot-password">Forgot Password</a>
+              </div>
+            </div>
+          </fieldset>
         </form> <!-- /container -->
+      </div>
 
     </main>
 
