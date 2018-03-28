578db49c6dd7c23e255c04c0d648500edf0b9571
Colin <ckingbailey@gmail.com>
2018-03-28 11:40:28 -0700

    Login flow (#5)
    * correct some typos in footer
    
    * checkout development config.php
    
    * rm jumbotron from login page
    
    * use offset-4 to center login form
    
    * vertical space login elements
    
    * make masthead logo and text smaller
    
    * tighten up layout of .page-header
    
    * rebase onto master

--- config.php
+++ config.php
@@ -1,9 +1,22 @@
 <?PHP
-/*Configuration Settings*/
+$actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-define('DB_Host', 'localhost'); /*Database Server*/
-define('DB_Name', 'svbxorg_defdb');  /*Database Name*/
-define('DB_USER', 'svbxorg_defdba'); /*Database Username*/
-define('DB_PWD', 'Berryessa2468!'); /*Database Password*/
+if (strpos($actual_link, '.c9') !== false) {
+    
+    /*C9 Configuration Settings*/
 
+    define('DB_Host', 'localhost'); /*Database Server*/
+    define('DB_Name', 'CDL');  /*Database Name*/
+    define('DB_USER', 'root'); /*Database Username*/
+    define('DB_PWD', ''); /*Database Password*/
+} else {
+    
+    /*Webserver Configuration Settings*/
+    
+    define('DB_Host', 'localhost'); /*Database Server*/
+    define('DB_Name', 'svbxorg_defdb');  /*Database Name*/
+    define('DB_USER', 'svbxorg_defdba'); /*Database Username*/
+    define('DB_PWD', 'Berryessa2468!'); /*Database Password*/
+    
+}
 ?>
