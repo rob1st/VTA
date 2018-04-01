53592b540121d8d22125f0f64c228df76049edc5
ckb <ckingbailey@gmail.com>
2018-04-01 01:15:24 +0000

    list-style-type:none and ul.grey-back

--- userAccount.php
+++ userAccount.php
@@ -12,25 +12,25 @@
     include('filestart.php');
     echo '
         <main class="container main-content">
-            <ul class="user-menu">
-                <li><a href="UpdateProfile.php">Update Profile</a></li>
-                <li><a href="UpdatePassword.php">Change Password</a></li>
+            <ul class="container grey-back user-menu">
+                <li class="user-menu-item"><a href="UpdateProfile.php">Update Profile</a></li>
+                <li class="user-menu-item"><a href="UpdatePassword.php">Change Password</a></li>
             </ul>
     ';
     if($role == 'A' OR 'S') {
         echo '
-            <ul class="user-menu admin-menu">
-                <li><a href="NewUser.php">Add User</a></li>
-                <li><a href="NewLocation.php">Add Location</a></li>
-                <li><a href="NewSystem.php">Add System</a></li>
+            <ul class="container grey-back user-menu admin-menu">
+                <li class="user-menu-item admin-menu-item"><a href="NewUser.php">Add User</a></li>
+                <li class="user-menu-item admin-menu-item"><a href="NewLocation.php">Add Location</a></li>
+                <li class="user-menu-item admin-menu-item"><a href="NewSystem.php">Add System</a></li>
             </ul>
         ';
         if ($role == 'S') {
             echo '
-                <ul class="user-menu superadmin-menu">
-                    <li><a href="NewEvidence.php">Add Evidence Type</a></li>
-                    <li><a href="NewSeverity.php">Add Severity Type</a></li>
-                    <li><a href="NewStatus.php">Add Status Type</a></li>
+                <ul class="container grey-back user-menu superadmin-menu">
+                    <li class="user-menu-item superadmin-menu-item"><a href="NewEvidence.php">Add Evidence Type</a></li>
+                    <li class="user-menu-item admin-menu-item"><a href="NewSeverity.php">Add Severity Type</a></li>
+                    <li class="user-menu-item admin-menu-item"><a href="NewStatus.php">Add Status Type</a></li>
                 </ul>
                 </main>
             ';
