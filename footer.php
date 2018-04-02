da587733dae4803338b37cbd59df649aaf7360f0
ckb <ckingbailey@gmail.com>
2018-04-02 19:45:42 +0000

    link script tag to d3js.org

--- footer.php
+++ footer.php
@@ -9,5 +9,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="/bootstrap-4-multi-dropdown-navbar.js"></script>
+    <script src="https://d3js.org/d3.v5.js"></script>
   </body>
 </html>
