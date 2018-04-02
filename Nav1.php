3cddadaafcb6d99fa5744fdb0e46cb8eafa9f59f
Colin <ckingbailey@gmail.com>
2018-04-02 12:14:26 -0700

    Userpage (#24)
    * make navbar-brand link to userAccount page
    
    * make userAcct and adminPage all one thing with conditionals
    
    * list-style-type:none and ul.grey-back
    
    * display role title
    
    * change color of acct-link:hover
    
    * change color of acct-link:hover to transparent
    
    * text-align center for all <h#> tags in .page-header
    
    * rm user page and admin page from navbar
    
    * add logout btn to bottom of user page
    
    * margin-bottom, rounded corners, and grey text in user page
    
    * center logout btn in container
    
    * slight variations in element spacing on user acct page
    
    * use cards for user-menu and change grey-back to grey-bg
    
    * style user-menus off BS .card styles
    
    * change navbar-heading text to contextual "Login Now" || $UserName
    
    * don' reuse var names (I need better linting)
    
    * call session_start at index.php, too
    
    * add contextual Logout btn to navbar

--- Nav1.php
+++ Nav1.php
@@ -3,7 +3,8 @@ require_once('SQLFunctions.php');
 
 if(!isset($_SESSION['UserID']))
 {
-    $login = 'Not logged in';
+    $login = 'Login now';
+    $logout = false;
 }
 else
 {
@@ -12,124 +13,118 @@ else
     $Username = $_SESSION['Username'];
     $Role = $_SESSION['Role'];
 
-    try
+try
+{
+     /*Connect to CRUD Database*/
+    $link = f_sqlConnect();
+
+    /* Prep SQL statement to find the user name based on the UserID */
+    $sql = "SELECT Username, firstname, lastname, Role FROM users_enc WHERE UserID = ".$UserID;
+
+    /*execute the sql statement*/
+    if($result=mysqli_query($link,$sql))
     {
-         /*Connect to CRUD Database*/
-        $link = f_sqlConnect();
-
-        /* Prep SQL statement to find the user name based on the UserID */
-        $sql = "SELECT Username, firstname, lastname, Role FROM users_enc WHERE UserID = ".$UserID;
-
-        /*execute the sql statement*/
-        if($result=mysqli_query($link,$sql))
-        {
-          /*from the sql results, assign the username that returned to the $username variable*/
-          while($row = mysqli_fetch_assoc($result)) {
-            $firstname = $row['firstname'];
-            $lastname = $row['lastname'];
-          }
-        }
-
-        if($Role=='S') {
-            $RoleT = 'Super Admin';
-        }
-        elseif($Role=='A') {
-            $RoleT = 'Administrator';
-        }
-        elseif($Role=='U') {
-            $RoleT = 'User';
-        }
-        elseif($Role=='V') {
-            $RoleT = 'Viewer';
-        }
-        else {
-            $RoleT = '';
-        }
-
-        /* Return Status to User*/
-        if($Username == false)
-        {
-            $login = 'Access Error<br />' .$RoleT;
-        }
-        else
-        {
-            $login = $firstname.' '.$lastname;
-        }
+      /*from the sql results, assign the username that returned to the $username variable*/
+      while($row = mysqli_fetch_assoc($result)) {
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
+      }
     }
-    /*if something goes wrong, return the following error*/
-    catch (Exception $e)
+
+    if($Role=='S') {
+        $RoleT = 'Super Admin';
+    }
+    elseif($Role=='A') {
+        $RoleT = 'Administrator';
+    }
+    elseif($Role=='U') {
+        $RoleT = 'User';
+    }
+    elseif($Role=='V') {
+        $RoleT = 'Viewer';
+    }
+    else {
+        $RoleT = '';
+    }
+
+    /* Return Status to User*/
+    if($Username == false)
     {
-        $login = 'Unable to process request.';
+        $login = 'Access Error<br />' .$RoleT;
     }
+    else
+    {
+        $login = $firstname.' '.$lastname;
+        $logout = true;
+    }
+}
+/*if something goes wrong, return the following error*/
+catch (Exception $e)
+{
+    $login = 'Unable to process request.';
+}
 }
 ?>
 <nav class="navbar navbar-expand-md navbar-dark navbar-vta-blue fixed-top">
-      <span class="navbar-brand navbar-heading"><?php echo $login; ?></span>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <span class="navbar-brand navbar-heading">
+    <?php
+      $navbarHref = 'login.php';
+      // if UserID is already set, link to userAccount page
+      if (isset($_SESSION['UserID'])) {
+        $navbarHref = 'userAccount.php';
+      }
+      echo '<a href="userAccount.php" class="navbar-link navbar-brand-link">'.$login.'</a>';
+    ?>
+  </span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <?php
-          if(!isset($_SESSION['UserID'])) {
-            echo "
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <?php
+      if($title == 'SVBX - Home') {
+        echo "
+      <li class='nav-item'>
+        <span class='nav-link disabled'>Home</span>
+      </li>";
+      } else {
+        echo "
+      <li class='nav-item'>
+        <a class='nav-link' href='stats.php'>Home</a>
+      </li>";
+      }
+      if($title == 'SVBX - Help') {
+        echo "
+      <li class='nav-item'>
+        <span class='nav-link disabled'>Help</span>
+      </li>";
+      } else {
+        echo "
+      <li class='nav-item'>
+        <a class='nav-link' href='help.php'>Help</a>
+      </li>";
+      }
+      if($Role == 'A' OR $Role == 'S' OR $Role == 'U' OR $Role == 'V') {
+        echo "
+      <li class='nav-item'>
+        <a class='nav-link' href='DisplayDefs.php'>Deficiencies</a>
+      </li>";
+      }
+      if($Role == 'A' OR $Role == 'S' OR $Role == 'U' OR $Role == 'V') {
+        echo "
           <li class='nav-item'>
-            <a class='nav-link' href='login.php'>Login</a>
+            <a class='nav-link' href='ViewSC.php'>Safety Certs</a>
           </li>";
-          } else {
-            echo "
-            <li class='nav-item'>
-            <a class='nav-link' href='logout.php'>Log out</a>
-          </li>";
-          }
-          if($title == 'SVBX - Home') {
-            echo "
-          <li class='nav-item'>
-            <span class='nav-link disabled'>Home</span>
-          </li>";
-          } else {
-            echo "
-          <li class='nav-item'>
-            <a class='nav-link' href='stats.php'>Home</a>
-          </li>";
-          }
-          if($title == 'SVBX - Help') {
-            echo "
-          <li class='nav-item'>
-            <span class='nav-link disabled'>Help</span>
-          </li>";
-          } else {
-            echo "
-          <li class='nav-item'>
-            <a class='nav-link' href='help.php'>Help</a>
-          </li>";
-          }
-          if($Role == 'A' OR $Role == 'S' OR $Role == 'U' OR $Role == 'V') {
-            echo "
-          <li class='nav-item'>
-            <a class='nav-link' href='DisplayDefs.php'>Deficiencies</a>
-          </li>";
-          }
-          if($Role == 'A' OR $Role == 'S') {
-            echo "
-          <li class='nav-item'>
-            <a class='nav-link' href='adminPage.php'>Admin</a>
-          </li>";
-          }
-          if($Role == 'A' OR $Role == 'S' OR $Role == 'U' OR $Role == 'V') {
-            echo "
-              <li class='nav-item'>
-                <a class='nav-link' href='ViewSC.php'>Safety Certs</a>
-              </li>";
-          }
-          if($Role == 'A' OR $Role == 'S' OR $Role == 'U' OR $Role == 'V') {
-            echo "
-              <li class='nav-item'>
-                <a class='nav-link' href='userAccount.php'>User Account</a>
-              </li>";
-          }
-          ?>
-        </ul>
-      </div>
-    </nav>
+      }
+      if($logout) {
+        echo '
+          <li class="nav-item">
+            <a class="nav-link" href="logout.php">Logout</a>
+          </li>
+        ';
+      }
+      ?>
+    </ul>
+  </div>
+</nav>
