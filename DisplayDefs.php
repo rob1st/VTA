3f4f499616ff79f59526052b9835ea9ff7ad5159
ckb <ckingbailey@gmail.com>
2018-04-03 19:03:20 +0000

    sort out thead and tbody in DisplayDefs

--- DisplayDefs.php
+++ DisplayDefs.php
@@ -22,66 +22,60 @@ include('filestart.php');
                         if ($Role == 'U' OR $Role == 'A' OR $Role == 'S') {
                             echo "<a href='NewDef.php' class='btn btn-primary'>Add New Deficiency</a>";
                         }
-        echo "
-            </div></div>
-            <ul class='def-nav'>
-                <li class='def-nav-item'><a href='DisplayDefs.php' class='btn btn-sm btn-outline def-nav-btn'>All</a></li>
-                <li class='def-nav-item'><a href='DisplayOpenDefs.php' class='btn btn-sm btn-outline def-nav-btn'>Open</a></li>
-                <li class='def-nav-item'><a href='DisplayClosedDefs.php' class='btn btn-sm btn-outline def-nav-btn'>Closed</a></li>
-            </ul>
-            
-            <table class='table table-responsive svbx-table def-table' border='1'>
-                <tbody>
+          echo "
+                </div></div>
+                <ul class='def-nav'>
+                    <li class='def-nav-item'><a href='DisplayDefs.php' class='btn btn-sm btn-outline def-nav-btn'>All</a></li>
+                    <li class='def-nav-item'><a href='DisplayOpenDefs.php' class='btn btn-sm btn-outline def-nav-btn'>Open</a></li>
+                    <li class='def-nav-item'><a href='DisplayClosedDefs.php' class='btn btn-sm btn-outline def-nav-btn'>Closed</a></li>
+                </ul>
+                
+                <table class='table table-striped table-responsive svbx-table def-table' border='1'>
                     <thead>
                         <tr class='svbx-tr table-heading'>
-                            <th class='svbx-th'>Def ID</th>
-                            <th class='collapse-sm collapse-xs svbx-th'>Location</th>
-                            <th class='collapse-xs svbx-th'>Severity</th>
-                            <th class='collapse-md  collapse-sm collapse-xs svbx-th'>Date Created</th>
-                            <th class='svbx-th'>Status</th>
-                            <th class='collapse-sm collapse-xs svbx-th'>System Affected</th>
-                            <th class='svbx-th'>Brief Description</th>";
+                            <th class='svbx-th id-th'>ID</th>
+                            <th class='svbx-th loc-th collapse-sm collapse-xs'>Location</th>
+                            <th class='svbx-th sev-th collapse-xs'>Severity</th>
+                            <th class='svbx-th date-th collapse-md  collapse-sm collapse-xs'>Date Created</th>
+                            <th class='svbx-th status-th'>Status</th>
+                            <th class='svbx-th system-th collapse-sm collapse-xs'>System Affected</th>
+                            <th class='svbx-th desrip-th'>Brief Description</th>";
                     if($Role == 'S' OR $Role == 'A' OR $Role == 'U') {
                         echo "
-                            <th class='collapse-md  collapse-sm collapse-xs svbx-th'>Last Updated</th>
-                            <th class='svbx-th'>Edit</th>";
-                    } else {
+                            <th class='svbx-th updated-th collapse-md  collapse-sm collapse-xs'>Last Updated</th>
+                            <th class='svbx-th edit-th'>Edit</th>";
+                    } else echo "</tr></thead><tbody>";
+                    if($Role == 'S') {
                         echo "
-                            </tr>";
-                        }
-                        if($Role == 'S') {
-                            echo "
-                                <th class='svbx-th'>Delete</th>
-                            </tr>"; 
-                        }
-                        while($row = mysqli_fetch_array($result)) {
-                            echo "
-                                <tr class='svbx-tr'>
-                                    <td class='svbx-td' style='text-align:center'><a href='ViewDef.php?DefID={$row[0]}' class='class1'>{$row[0]}</a></td>
-                                <td class='collapse-sm collapse-xs svbx-td'>{$row[1]}</td>
-                                <td class='collapse-xs svbx-td' style='text-align:center'>{$row[2]}</td>
-                                <td class='collapse-md  collapse-sm collapse-xs svbx-td' style='text-align:center'>{$row[3]}</td>
-                                <td class='svbx-td' style='text-align:center'>{$row[4]}</td>
-                                <td class='collapse-sm collapse-xs svbx-td'>{$row[5]}</td>
-                                <td class='svbx-td'>"; echo nl2br($row[6]);
-                            echo "</td>";
-                        if($Role == 'S' OR $Role == 'A' OR $Role == 'U') 
-                        {
-                            echo "
+                            <th class='svbx-th'>Delete</th>
+                        </tr></thead>"; 
+                    } else echo '</tr></thead><tbody>';
+                    
+                while($row = mysqli_fetch_array($result)) {
+                    echo "
+                        <tr class='svbx-tr'>
+                            <td class='svbx-td'><a href='ViewDef.php?DefID={$row[0]}' class='class1'>{$row[0]}</a></td>
+                            <td class='collapse-sm collapse-xs svbx-td'>{$row[1]}</td>
+                            <td class='collapse-xs svbx-td'>{$row[2]}</td>
+                            <td class='collapse-md  collapse-sm collapse-xs svbx-td'>{$row[3]}</td>
+                            <td class='svbx-td'>{$row[4]}</td>
+                            <td class='collapse-sm collapse-xs svbx-td'>{$row[5]}</td>
+                            <td class='svbx-td'>".nl2br($row[6])."</td>";
+                    if ($Role == 'S' OR $Role == 'A' OR $Role == 'U') {
+                       echo "
                             <td class='collapse-md  collapse-sm collapse-xs svbx-td'>{$row[7]}</td>
-                            <td class='svbx-td' style='text-align:center'><form action='UpdateDef.php' method='POST' onsubmit=''/>
-                            <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-edit'></i></button></form></td>";
-                        } else {
-                            echo "</tr>";
-                        }
-                        if($Role == 'S') {
-                            echo "
-                                <td class='svbx-td' style='text-align:center'><form action='DeleteDef.php' method='POST' onsubmit='' onclick='return confirm(`ARE YOU SURE? Deficiencies should not be deleted, your deletion will be logged.`)'/>
-                                    <button type='Submit' name='q' value='".$row[0]."'><i class='typcn typcn-times'></i></form></td>
-                                </tr>";
-                        }
+                            <td class='svbx-td'><form action='UpdateDef.php' method='POST' onsubmit=''/>
+                                <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-edit'></i></button></form>
+                            </td>";
+                    } else echo "</tr>";
+                    if ($Role == 'S') {
+                        echo "
+                            <td class='svbx-td'><form action='DeleteDef.php' method='POST' onsubmit='' onclick='return confirm(`ARE YOU SURE? Deficiencies should not be deleted, your deletion will be logged.`)'/>
+                                <button type='Submit' name='q' value='".$row[0]."'><i class='typcn typcn-times'></i></form></td>
+                            </tr>";
+                    }
             }
-                echo "</table></main>";
+        echo "</tbody></table></main>";
     }
 //mysqli_free_result($result);
 
