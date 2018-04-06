0fcc5658100ed986a319891fb69d650c94e88549
ckb <ckingbailey@gmail.com>
2018-04-06 17:44:57 +0000

    replace table elements with divs in NewDef

--- NewDef.php
+++ NewDef.php
@@ -13,246 +13,155 @@ include('filestart.php') ?>
     </header>
     <main class='container main-content'>
         <form action="RecDef.php" method="POST">
-            <table class='table svbx-table'>
-                <tr class='vdtr'>
-                    <th colspan='4' class='vdth'>
-                        <p>New Deficiency</p>
-                    </th>
-                </tr>
-                <tr class='vdtr'>
-                    <th colspan='4' class='vdth'>
-                        <p>Required Information</p>
-                    </th>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>Affect Safety Cert:</p>
-                    </td>
-                    <td class='vdtd'>
-                        <select name'SafetyCert' value='' id='defdd' required></option>
-                        <option value=''></option>
-                        <option value='Y'>Yes</option>
-                        <option value='N'>No</option>
-                    </td>
-                    <td class='vdtd'>
-                        <p>System Affected</p>
-                    </td>
-                    <td class='vdtd'>
+            <h5 class="bg-secondary text-white form-section-heading">Required Information</h5>
+            <section class="form-section">
+                <div class="form-row">
+                    <label>Affect Safety Cert:
+                        <select name'SafetyCert' value='' id='defdd' required>
+                            <option value=''></option>
+                            <option value='Y'>Yes</option>
+                            <option value='N'>No</option>
+                        </select>
+                    </label>
+                    <label>System Affected
                         <?php
                             $sqlY = "SELECT SystemID, System FROM System ORDER BY System";
                              //if($result = mysqli_query($link,$sqlY)) {
-                                    echo "<select name='SystemAffected' value='' id='defdd' required></option>";
+                                    echo "<select name='SystemAffected' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlY) as $row) {
                                         echo "<option value=$row[SystemID]>$row[System]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>General Location:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                </div>
+                <div class="form-row">
+                    <label>General Location:
                         <?php
                             $sqlL = "SELECT LocationID, LocationName FROM Location ORDER BY LocationName";
                              //if($result = mysqli_query($link,$sqlL)) {
-                                    echo "<select name='Location' value='' id='defdd' required></option>";
+                                    echo "<select name='Location' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlL) as $row) {
                                         echo "<option value=$row[LocationID]>$row[LocationName]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Specific Location:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                    <label>Specific Location:
                         <input type="text" name="SpecLoc" max="55" id="defdd" required/>
-                    </td>
-                </tr>
-                <tr class='vdtr'>    
-                    <td class='vdtd' id='status'>
-                        <p>Status:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                </div>
+                <div class="form-row">
+                    <label>Status:
                         <?php
                             $sqlT = "SELECT StatusID, Status FROM Status WHERE StatusID <> '3' ORDER BY StatusID";
                              //if($result = mysqli_query($link,$sqlT)) {
-                                    echo "<select name='Status' value='' id='defdd' required></option>";
+                                    echo "<select name='Status' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlT) as $row) {
                                         echo "<option value=$row[StatusID]>$row[Status]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Severity</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                    <label>Severity
                         <?php
                             $sqlS = "SELECT SeverityID, SeverityName FROM Severity ORDER BY SeverityName";
                              //if($result = mysqli_query($link,$sqlS)) {
-                                    echo "<select name='Severity' value='' id='defdd' required></option>";
+                                    echo "<select name='Severity' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlS) as $row) {
                                         echo "<option value=$row[SeverityID]>$row[SeverityName]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                </tr>
-                <tr class='vdtr'>    
-                    <td class='vdtd'>
-                        <p>To be resolved by:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                </div>
+                <div class="form-row">
+                    <label>To be resolved by:
                         <input type='date' name='DueDate' id='defdd'/>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Required for:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                    <label>Required for:
                         <?php
                             $sqlRB = "SELECT ReqByID, RequiredBy FROM RequiredBy ORDER BY ReqByID";
                              //if($result = mysqli_query($link,$sqlS)) {
-                                    echo "<select name='RequiredBy' value='' id='defdd' required></option>";
+                                    echo "<select name='RequiredBy' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlRB) as $row) {
                                         echo "<option value=$row[ReqByID]>$row[RequiredBy]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                </tr>
-                <tr class='vdtr'>    
-                    <td class='vdtd'>
-                        <p>Group to resolve:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                </div>
+                <div class="form-row">
+                    <label>Group to resolve:
                         <?php
                             $sqlY = "SELECT SystemID, System FROM System ORDER BY System";
                              //if($result = mysqli_query($link,$sqlY)) {
-                                    echo "<select name='GroupToResolve' value='' id='defdd' required></option>";
+                                    echo "<select name='GroupToResolve' value='' id='defdd' required>";
                                     echo "<option value=''></option>";
                                     foreach(mysqli_query($link,$sqlY) as $row) {
                                         echo "<option value=$row[SystemID]>$row[System]</option>";
                              }
                             echo "</select>";
                         ?>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Identified by:</p>
-                    </td>
-                    <td class='vdtd'>
+                    </label>
+                    <label>Identified by:
                         <input type="text" name="IdentifiedBy" max="24" id="defdd" required/>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td colspan='4' style="text-align:center"  class='vdtd'>
-                        <p>Deficiency Description</p>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td Colspan=4>
-                        <textarea type="message"  rows="5" cols="99%" name="description" max="1000" required></textarea>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <th colspan='4' class='vdth'>
-                        <p>Optional Information</p>
-                    </th>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>Spec or Code:</p>
-                    </td>
-                    <td colspan="3" class='vdtd'>
-                        <input type="text" name="Spec" max="55"  id="defdd"/>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>Action Owner:</p>
-                    </td>
-                    <td class='vdtd'>
-                        <input type="text" name="ActionOwner" max="24"  id="defdd"/>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Old Id:</p>
-                    </td>
-                    <td class='vdtd'>
-                        <input type="text" name="OldID" max="24" id="defdd"/>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td colspan='4' style="text-align:center"  class='vdtd'>
-                        <p>More Information</p>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td Colspan=4 class='vdtd'>
-                        <textarea type="message"  rows="5" cols="99%" name="comments" max="1000"></textarea>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <th colspan='4' class='vdth'>
-                        <p>Closure Information</p>
-                    </th>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>Evidence Type:</p>
-                    </td>
-                    <td class='vdtd' colspan='3'>
-                        <?php
-                            $sqlE = "SELECT EviTypeID, EviType FROM EvidenceType ORDER BY EviType";
-                             //if($result = mysqli_query($link,$sqlE)) {
-                                    echo "<select name='EvidenceType' value='' id='defdd'></option>";
-                                    echo "<option value=''></option>";
-                                    foreach(mysqli_query($link,$sqlE) as $row) {
-                                        echo "<option value=$row[EviTypeID]>$row[EviType]</option>";
-                             }
-                            echo "</select>";
-                        ?>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td class='vdtd'>
-                        <p>Evidence Repository:</p>
-                    </td>
-                    <td class='vdtd'>
-                        <select name'Repo' value='' id='defdd'></option>
+                    </label>
+                </div>
+                <label>Deficiency Description
+                    <textarea type="message"  rows="5" cols="99%" name="description" max="1000" required></textarea>
+                </label>
+            </section>
+            <h5 class="bg-secondary text-white form-section-heading">Optional Information</h5>
+            <section class="form-section">
+                <label>Spec or Code:
+                    <input type="text" name="Spec" max="55"  id="defdd"/>
+                </label>
+                <label>Action Owner:
+                    <input type="text" name="ActionOwner" max="24"  id="defdd"/>
+                </label>
+                <label>Old Id:
+                    <input type="text" name="OldID" max="24" id="defdd"/>
+                </label>
+                <label>More Information
+                    <textarea type="message"  rows="5" cols="99%" name="comments" max="1000"></textarea>
+                </label>
+            </section>
+            <h5 class="bg-secondary text-white form-section-heading">Closure Information</h5>
+            <section class="form-section">
+                <label>Evidence Type:
+                    <?php
+                        $sqlE = "SELECT EviTypeID, EviType FROM EvidenceType ORDER BY EviType";
+                         //if($result = mysqli_query($link,$sqlE)) {
+                                echo "<select name='EvidenceType' value='' id='defdd'></option>";
+                                echo "<option value=''></option>";
+                                foreach(mysqli_query($link,$sqlE) as $row) {
+                                    echo "<option value=$row[EviTypeID]>$row[EviType]</option>";
+                         }
+                        echo "</select>";
+                    ?>
+                </label>
+                <label>Evidence Repository:
+                    <select name'Repo' value='' id='defdd'>
                         <option value=''></option>
                         <option value='A'>Aconex</option>
                         <option value='S'>SharePoint</option>
-                    </td>
-                    <td class='vdtd'>
-                        <p>Repository Number</p>
-                    </td>
-                    <td class='vdtd'>
-                        <input type="text" name="EvidenceLink" max="255"  id='defdd'/>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td colspan='4' style="text-align:center"  class='vdtd'>
-                        <p>Closure Comments</p>
-                    </td>
-                </tr>
-                <tr class='vdtr'>
-                    <td Colspan=4 class='vdtd'>
-                        <textarea type="message"  rows="5" cols="99%" name="ClosureComments" max="1000"></textarea>
-                    </td>
-                </tr>
-            </table>
-            </div><br>
-            <div  style='display: flex; align-items: center; justify-content: center; hspace:20'>
-            <input type='submit' value='submit' class='btn btn-primary btn-lg' /><p> </p>
-            <div style='width:5px; height:auto; display:inline-block'></div>
-            <input type='reset' value='reset' class='btn btn-primary btn-lg' />
+                    </select>
+                </label>
+                <label>Repository Number
+                    <input type="text" name="EvidenceLink" max="255"  id='defdd'/>
+                </label>
+                <label>Closure Comments
+                    <textarea type="message"  rows="5" cols="99%" name="ClosureComments" max="1000"></textarea>
+                </label>
+                <input type='submit' value='submit' class='btn btn-primary btn-lg' />
+                <input type='reset' value='reset' class='btn btn-primary btn-lg' />
+            </section>
         </form>
     </main>
 <?php 
