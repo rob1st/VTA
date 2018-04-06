fe04576a5b2ca0050d3f8f72aa5ac53be1b3ef6e
ckb <ckingbailey@gmail.com>
2018-04-06 22:17:39 +0000

    use flex: column to center textarea

--- NewDef.php
+++ NewDef.php
@@ -20,6 +20,7 @@ include('filestart.php')
                 <div class="form-element">
                     <label class="input-label">Affect Safety Cert</label>
                     <select name'SafetyCert' id='defdd' required class="flex-input">
+                        <option value='' disabled selected>Y/N</option>
                         <option value='Y'>Yes</option>
                         <option value='N'>No</option>
                     </select>
@@ -29,7 +30,8 @@ include('filestart.php')
                     <?php
                         $sqlY = "SELECT SystemID, System FROM System ORDER BY System";
                          //if($result = mysqli_query($link,$sqlY)) {
-                                echo "<select name='SystemAffected' id='defdd' required class='flex-input'>";
+                                echo "<select name='SystemAffected' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Choose System</option>";
                                 foreach(mysqli_query($link,$sqlY) as $row) {
                                     echo "<option value={$row[SystemID]}>{$row[System]}</option>";
                          }
@@ -43,7 +45,8 @@ include('filestart.php')
                     <?php
                         $sqlL = "SELECT LocationID, LocationName FROM Location ORDER BY LocationName";
                          //if($result = mysqli_query($link,$sqlL)) {
-                                echo "<select name='Location' id='defdd' required class='flex-input'>";
+                                echo "<select name='Location' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Choose Location</option>";
                                 foreach(mysqli_query($link,$sqlL) as $row) {
                                     echo "<option value={$row[LocationID]}>{$row[LocationName]}</option>";
                          }
@@ -61,7 +64,8 @@ include('filestart.php')
                     <?php
                         $sqlT = "SELECT StatusID, Status FROM Status WHERE StatusID <> '3' ORDER BY StatusID";
                          //if($result = mysqli_query($link,$sqlT)) {
-                                echo "<select name='Status' id='defdd' required class='flex-input'>";
+                                echo "<select name='Status' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Choose Status</option>";
                                 foreach(mysqli_query($link,$sqlT) as $row) {
                                     echo "<option value={$row[StatusID]}>{$row[Status]}</option>";
                          }
@@ -73,7 +77,8 @@ include('filestart.php')
                     <?php
                         $sqlS = "SELECT SeverityID, SeverityName FROM Severity ORDER BY SeverityName";
                          //if($result = mysqli_query($link,$sqlS)) {
-                                echo "<select name='Severity' id='defdd' required class='flex-input'>";
+                                echo "<select name='Severity' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Choose Severity</option>";
                                 foreach(mysqli_query($link,$sqlS) as $row) {
                                     echo "<option value={$row[SeverityID]}>{$row[SeverityName]}</option>";
                          }
@@ -91,7 +96,8 @@ include('filestart.php')
                     <?php
                         $sqlRB = "SELECT ReqByID, RequiredBy FROM RequiredBy ORDER BY ReqByID";
                          //if($result = mysqli_query($link,$sqlS)) {
-                                echo "<select name='RequiredBy' id='defdd' required class='flex-input'>";
+                                echo "<select name='RequiredBy' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Requirement</option>";
                                 foreach(mysqli_query($link,$sqlRB) as $row) {
                                     echo "<option value=$row[ReqByID]>$row[RequiredBy]</option>";
                          }
@@ -105,7 +111,8 @@ include('filestart.php')
                     <?php
                         $sqlY = "SELECT SystemID, System FROM System ORDER BY System";
                          //if($result = mysqli_query($link,$sqlY)) {
-                                echo "<select name='GroupToResolve' id='defdd' required class='flex-input'>";
+                                echo "<select name='GroupToResolve' id='defdd' required class='flex-input'>
+                                        <option value='' disabled selected>Group</option>";
                                 foreach(mysqli_query($link,$sqlY) as $row) {
                                     echo "<option value=$row[SystemID]>$row[System]</option>";
                          }
@@ -118,7 +125,7 @@ include('filestart.php')
                 </div>
             </div>
             <div class="constrainer form-subsection center-element">
-                <div class="form-element">
+                <div class="form-element flex-column">
                     <label class="input-label label-breakline">Deficiency Description</label>
                     <textarea name="description" rows="5" maxlength="1000" required class="textarea-full-width"></textarea>
                 </div>
@@ -143,7 +150,7 @@ include('filestart.php')
                 </div>
             </div>
             <div class="constrainer form-subsection center-element">
-                <div class="form-element">
+                <div class="form-element flex-column">
                     <label class="input-label label-breakline">More Information</label>
                     <textarea name="comments" rows="5" maxlength="1000" class="textarea-full-width"></textarea>
                 </div>
@@ -157,7 +164,8 @@ include('filestart.php')
                     <?php
                         $sqlE = "SELECT EviTypeID, EviType FROM EvidenceType ORDER BY EviType";
                          //if($result = mysqli_query($link,$sqlE)) {
-                                echo "<select name='EvidenceType' id='defdd' class='flex-input'></option>";
+                                echo "<select name='EvidenceType' id='defdd' class='flex-input'></option>
+                                        <option value='' disabled selected>Choose Type</option>";
                                 foreach(mysqli_query($link,$sqlE) as $row) {
                                     echo "<option value=$row[EviTypeID]>$row[EviType]</option>";
                          }
@@ -169,6 +177,7 @@ include('filestart.php')
                 <div class="form-element">
                     <label class="input-label">Evidence Repository</label>
                     <select name'Repo' id='defdd' class="flex-input">
+                        <option value='' disabled selected>Choose Repo</option>
                         <option value=''></option>
                         <option value='A'>Aconex</option>
                         <option value='S'>SharePoint</option>
@@ -180,13 +189,13 @@ include('filestart.php')
                 </div>
             </div>
             <div class="constrainer form-subsection center-element">
-                <div class="form-element">
+                <div class="form-element flex-column">
                     <label class="input-label label-breakline">Closure Comments</label>
                     <textarea name="ClosureComments" rows="5" maxlength="1000" class="textarea-full-width"></textarea>
                 </div>
             </div>
         </fieldset>
-        <fieldset>
+        <fieldset class="center-content">
             <button type='submit' value='submit' class='btn btn-primary btn-lg'>Submit</button>
             <button type='reset' value='reset' class='btn btn-primary btn-lg'>Reset</button>
         </fieldset>
