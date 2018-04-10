6e0d89450c96b1e3d8eb439b7ee80b1ccf379e47
ckb <ckingbailey@gmail.com>
2018-04-10 17:24:08 +0000

    apply bootstrap card styles to first two fieldsets

--- dailyReport.php
+++ dailyReport.php
@@ -12,44 +12,52 @@ $reptNum = int; // generate this based on db
     <form action='submitDaily.php' method='POST'>
         <div class='flex-row space-between'>
             <fieldset id='dayData' class='card half-container'>
-                <div class='flex-row no-wrap space-between align-center grey-bg'>
-                    <label class='form-section-heading flex-grow item-margin-right'>Report #</label>
-                    <input type='text' id='reportNum' class='form-control' />
+                <div class='card-header grey-bg'>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Report #</label>
+                        <input type='text' id='reportNum' class='form-control' />
+                    </div>
                 </div>
-                <div class='flex-row no-wrap space-between align-center'>
-                    <label class='flex-grow item-margin-right'>Contract day</label>
-                    <input type='text' id='contractDay' class='form-control' />
-                </div>
-                <div class='flex-row no-wrap space-between align-center'>
-                    <label class='flex-grow item-margin-right'>Date</label>
-                    <input type='text' value='${curDate}' id='curDate' class='form-control' readonly />
-                </div>
-                <div class='flex-row no-wrap space-between align-center'>
-                    <label class='flex-grow item-margin-right'>Project</label>
-                    <input type='text' id='projectId' class='form-control' />
-                </div>
-                <div class='flex-row no-wrap space-between align-center'>
-                    <label class='flex-grow item-margin-right'>Weather</label>
-                    <input type='text' id='weatherDescrip' class='form-control' />
-                </div>
-                <div class='flex-row no-wrap space-between align-center'>
-                    <label class='flex-grow item-margin-right'>Shift Hrs</label>
-                    <input type='text' id='shiftHrs' class='form-control' />
+                <div class='card-body'>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Contract day</label>
+                        <input type='text' id='contractDay' class='form-control' />
+                    </div>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Date</label>
+                        <input type='text' value='${curDate}' id='curDate' class='form-control' readonly />
+                    </div>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Project</label>
+                        <input type='text' id='projectId' class='form-control' />
+                    </div>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Weather</label>
+                        <input type='text' id='weatherDescrip' class='form-control' />
+                    </div>
+                    <div class='flex-row no-wrap space-between align-center item-margin-bottom'>
+                        <label class='input-label item-margin-right'>Shift Hrs</label>
+                        <input type='text' id='shiftHrs' class='form-control' />
+                    </div>
                 </div>
             </fieldset>
             <fieldset id='safetyData' class='card half-container'>
-                <h5 class='form-section-heading grey-bg'>Safety</h5>
-                <div class='flex-row no-wrap space-between'>
-                    <label>EIC</label>
-                    <input type='text' id='eic' class='form-control' />
+                <div class='card-header grey-bg'>
+                    <h5 class='form-section-heading grey-bg'>Safety</h5>
                 </div>
-                <div class='flex-row no-wrap space-between'>
-                    <label>Watchman</label>
-                    <input type='text' id='watchman' class='form-control' />
-                </div>
-                <div class='flex-row no-wrap space-between'>
-                    <label>RAP #</label>
-                    <input type='text' id='rapNum' class='form-control' />
+                <div class='card-body'>
+                    <div class='flex-row no-wrap space-between'>
+                        <label class='input-label'>EIC</label>
+                        <input type='text' id='eic' class='form-control' />
+                    </div>
+                    <div class='flex-row no-wrap space-between'>
+                        <label class='input-label'>Watchman</label>
+                        <input type='text' id='watchman' class='form-control' />
+                    </div>
+                    <div class='flex-row no-wrap space-between'>
+                        <label class='input-label'>RAP #</label>
+                        <input type='text' id='rapNum' class='form-control' />
+                    </div>
                 </div>
             </fieldset>
         </div>
