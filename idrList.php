ec8806e6fb4d074d0e1ec5380fca1a02392eb271
ckb <ckingbailey@gmail.com>
2018-05-03 19:21:07 +0000

    conditionally render tab buttons if user has own IDRs

--- idrList.php
+++ idrList.php
@@ -7,10 +7,14 @@ $role = $_SESSION['Role'];
 
 $link = f_sqlConnect();
 $qry = "SELECT idrID, UserID, idrDate FROM IDR";
+
 $errorMsg = [
     'myIDRs' => 'Unable to retrieve reports for user',
     'idrList' => 'There was a problem retrieving report list'
 ];
+
+$myIDRs = $link->query("$qry WHERE UserID='$userID'");
+if ($link->error) $myIDRs = $errorMsg['myIDRs'];
 
 include 'filestart.php';
 ?>
@@ -18,13 +22,20 @@ include 'filestart.php';
     <h1 class='page-title'>Inspectors' Daily Reports</h1>
 </header>
 <main class='container main-content'>
+    <!--tab controls-->
     <div class='row'>
         <div class='col-6'>
             <h4 class='text-center'><a role='button' href='#allReports'>All Reports</a></h4>
         </div>
-        <div class='col-6'>
-            <h4 class='text-center'><a role='button' href='#myReports'>My Reports</a></h4>
-        </div>
+        <?php
+            // render My IDRs button only if user has IDRs of their own
+            if ($myIDRs) {
+                echo "
+                    <div class='col-6'>
+                        <h4 class='text-center'><a role='button' href='#myReports'>My Reports</a></h4>
+                    </div>";
+            }
+        ?>
     </div>
     <div id='allReports' class='row'>
         <div class='col-12'>
@@ -46,23 +57,24 @@ include 'filestart.php';
         </div>
     </div>
     <?php
-        // if user has an IDRs of their own render a button to show them
-        if ($result = $link->query("$qry WHERE UserID='$userID'")) {
-            if ($result->num_rows) {
+        // if user has an IDRs of their own render them under My IDRs tab
+        if ($myIDRs) {
+            if ($myIDRs === $errorMsg['myIDRs']) echo "<h4 class='text-secondary'>{$errorMsg['myIDRs']}</h4>";
+            elseif ($myIDRs->num_rows) {
                 echo "
                     <div id='myReports' class='row'>
                         <div class='col-12'>
                             <ul>";
-                            while ($row = $result->fetch_assoc()) {
+                            while ($row = $myIDRs->fetch_assoc()) {
                                 printf("<li><a href='%s'>%s</a></li>", "/idr.php?idrID={$row['idrID']}", $row['idrDate']);
                             }
                 echo "
                             </ul>
                         </div>
                     </div>";
+                $myIDRs->close();
             }
-            $result->close();
-        } elseif ($link->error) echo "<h4 class='text-secondary'>{$errorMsg['myIDRs']}</h4>";
+        }
     ?>
 </main>
 <?php
