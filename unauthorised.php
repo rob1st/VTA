c0dbea53864a5c95955e23f732282402ceeb7862
ckb <ckingbailey@gmail.com>
2018-05-04 22:08:14 +0000

    fix diff in unauthorised.php

--- unauthorised.php
+++ unauthorised.php
@@ -1,11 +1,7 @@
 <?php
     $title = "SVBX - Unauthorised";
-<<<<<<< HEAD
-    include('filestart.php') 
-=======
     include('filestart.php');
     http_response_code(401);
->>>>>>> development
 ?>
     <header class="container page-header">
         <h1 class="page-title">Unauthorised Access Denied</h1>
