ed9c1167b150bdb1696a475744e72838ec909b9a
ckb <ckingbailey@gmail.com>
2018-05-08 18:17:33 +0000

    use json_encoded data and generic fcn to draw pie charts

--- js/pie_chart.js
+++ js/pie_chart.js
@@ -3,6 +3,41 @@
     - dataToRender will be passed from php MySQL query
     - this will require refactoring fileend.php into a fcn
 */
+function drawPieChart(container, jsonData, colorData, d3 = window.d3) {
+    console.log(jsonData);
+    const w = '200';
+    const h = '200';
+    const r = Math.min(w, h)/2;
+    
+    // append color scheme to jsonData
+    // for (let obj of jsonData) {
+    //     obj.color = colorData[obj.label] || colorData[jsonData.indexOf(obj)];
+    // }
+    const color = d3.scaleOrdinal(Object.values(colorData));
+    
+    const svg = d3.select(container)
+        .append('svg')
+        .attr('width', w)
+        .attr('height', h)
+        .append('g')
+        .attr('transform', `translate(${w/2},${h/2})`)
+        
+    const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(r)
+        
+    var pie = d3.pie()
+        .value(d => d.value)
+        .sort(null)
+        
+    svg.selectAll('path')
+        .data(pie(jsonData))
+        .enter()
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', d => color(d.data.label))
+}
+
 function drawOpenCloseChart(d3, open, closed) {
     var openCloseData = [
         {label: 'open', count: open},
--- stats.php
+++ stats.php
@@ -17,13 +17,23 @@ session_start();
     $sqlLoc = "SELECT COUNT(*) FROM Location"; //Location Counts
     $sqlET = "SELECT COUNT(*) FROM CDL WHERE Status=2"; //Status Closed Counts
         
-    // vars to pass to JS scripts    
-    $statusOpen = 0;
-    $statusClosed = 0;
-    $blockSev = 0;
-    $critSev = 0;
-    $majSev = 0;
-    $minSev = 0;
+    // vars to pass to JS scripts
+    $statusData = [
+      open => '',
+      closed => ''
+    ];
+    $sevData = [
+      blocker => '',
+      critical => '',
+      major => '',
+      minor => ''
+    ];
+    // $statusOpen = 0;
+    // $statusClosed = 0;
+    // $blockSev = 0;
+    // $critSev = 0;
+    // $majSev = 0;
+    // $minSev = 0;
 
     //if(!f_tableExists($link, $table, DB_Name)) {
     //    die('<br>Destination table does not exist: '.$table);
@@ -34,8 +44,8 @@ session_start();
     // if any extra content is needed, add it to index after required items
     $cards = [
       ['Status', 'Statuses', 'Items', 
-        "<div class='data-display'><div id='open-closed-graph' class='chart-container'></div><p id='open-closed-legend' class='flex-column'></p></div>"],
-      ['Severity', 'Severities', 'Open Items', '<div class="data-display"><div id="severity-graph" class="chart-container"></div><p id="open-closed-legend" class="legend"></p></div>'],
+        "<div class='data-display'><div id='status-graph' class='chart-container'></div><p id='status-legend' class='flex-column'></p></div>"],
+      ['Severity', 'Severities', 'Open Items', '<div class="data-display"><div id="severity-graph" class="chart-container"></div><p id="severity-legend" class="legend"></p></div>'],
       ['System', 'Systems', 'Actions'],
       ['Location', 'Locations', 'Open Items']
     ];
@@ -46,42 +56,35 @@ session_start();
       'Location' => 'SELECT L.LocationName, COUNT(C.Status) FROM CDL C LEFT JOIN Location L ON L.LocationID=C.Location WHERE Status = 1 GROUP BY Location  ORDER BY L.LocationName'
     ];
     
-    function writeDashCard($tot, $qry, $cardSpecs) {
-      global $statusOpen, $statusClosed, $blockSev, $critSev, $majSev, $minSev;
-      if ($db) $dbConnect = 'connection successful';
+    function writeDashCard($count, $result, $card) {
+      global $statusData, $sevData;
+      // global $statusOpen, $statusClosed, $blockSev, $critSev, $majSev, $minSev;
+      // if ($db) $dbConnect = 'connection successful';
       echo "
         <div class='card dash-card'>
           <header class='card-header'>
-            <p style='margin: 0; font-size: .75rem; color: crimson'>{$dbConnect}</p>
-            <h4>{$cardSpecs[1]}</h4>
+            <h4>{$card[1]}</h4>
           </header>
           <div class='card-body grey-bg'>
             <ul class='dash-list'>
               <li class='bg-secondary text-white dash-list-heading'>
-                <span class='dash-list-left dash-list-name'>{$cardSpecs[0]}</span>
-                <span class='dash-list-right dash-list-count'>{$cardSpecs[2]}</span>
+                <span class='dash-list-left dash-list-name'>{$card[0]}</span>
+                <span class='dash-list-right dash-list-count'>{$card[2]}</span>
               </li>";
-      if ($tot && $qry) {
-        while ($row = mysqli_fetch_array($qry)) {
-          if ($row[0] == 'Open') $statusOpen = $row[1];
-          elseif ($row[0] == 'Closed') $statusClosed = $row[1];
-          elseif ($row[0] == 'Blocker') $blockSev = $row[1];
-          elseif ($row[0] == 'Critical') $critSev = $row[1];
-          elseif ($row[0] == 'Major') $majSev = $row[1];
-          elseif ($row[0] == 'Minor') $minSev = $row[1];
+      if ($count && $result) {
+        while ($row = $result->fetch_array()) {
+          if ($row[0] === 'Open' || $row[0] === 'Closed') $statusData[lcfirst($row[0])] = $row[1];
+          elseif ($row[0] === 'Blocker' || $row[0] === 'Critical' || $row[0] === 'Major' || $row[0] === 'Minor') $sevData[lcfirst($row[0])] = $row[1];
           echo "
               <li class='dash-list-item'>
                 <span class='dash-list-left'>{$row[0]}</span>
                 <span class='dash-list-right'>{$row[1]}</span>
               </li>
           ";
-          if ($row[1] == 2) {
-            echo "<script>console.log('{$row[0]}: ', '{$row[1]}');</script>";
-          }
         }
         echo "</ul>";
-        if (count($cardSpecs) > 3) {
-          echo "{$cardSpecs[3]}</div>";
+        if (count($card) > 3) {
+          echo "{$card[3]}</div>";
         } else echo "</div>";
         echo "
             <footer class='card-footer'>
@@ -101,21 +104,35 @@ session_start();
   <?php
   foreach($cards as $card) {
     $tableStr = 'SELECT COUNT(*) FROM '.$card[0];
-    $count = mysqli_fetch_array(mysqli_query($link, $tableStr))[0];
-    $cardData = mysqli_query($link, $queries[$card[0]]);
-    writeDashCard($count, $cardData, $card);
+    $count = $link->query($tableStr)->fetch_array()[0];
+    $result = $link->query($queries[$card[0]]);
+    writeDashCard($count, $result, $card);
   }
 ?> 
 </main>
 <?php
+  // replace flat $___Data props with d3-structured objs
+  foreach ($statusData as $name => $num) {
+    array_push($statusData, [ label => $name, value => $num ]);
+    unset($statusData[$name]);
+  }
+  $jsonStatus = json_encode($statusData);
+  $statusColors = json_encode([ red => '#d73027', green => '#58BF73' ]);
+  
+  foreach ($sevData as $name => $num) {
+    array_push($sevData, [ label => $name, value => $num ]);
+    unset($sevData[$name]);
+  }
+  $jsonSev = json_encode($sevData);
+  $sevColors = json_encode([ red => '#bd0026', redOrange => '#fc4e2a', orange => '#feb24c', yellow => '#ffeda0']);
   echo "
   <!--THIS IS A TERRIBLE WAY TO DO THIS
       THIS IS ONLY FOR TESTING PURPOSES-->
   <script src='https://d3js.org/d3.v5.js'></script>
   <script src='js/pie_chart.js'></script>
   <script>
-    window.drawOpenCloseChart(window.d3, '".$statusOpen."', '".$statusClosed."')
-    window.drawSeverityChart(window.d3, '".$blockSev."', '".$critSev."', '".$majSev."', '".$minSev."')
+    window.drawPieChart(document.getElementById('status-graph'), $jsonStatus, $statusColors)
+    window.drawPieChart(document.getElementById('severity-graph'), $jsonSev, $sevColors)
   </script>
   <!--REMOVE ABOVE SCRIPT TAGS ONCE TESTING IS DONE-->
   <!--DO NOT TYPE BELOW THIS LINE-->";
