3aeca3bb1f938a642a867f0585040a7c602b5e02
ckb <ckingbailey@gmail.com>
2018-05-09 18:56:51 +0000

    refactor Defs Table into a fcn

--- DisplayDefs.php
+++ DisplayDefs.php
@@ -180,6 +180,69 @@ function printProjectSearchBar($cnxn, $formAction) {
     return $form;
 }
 
+function printProjectDefsTable($cnxn, $qry, $lvl) {
+    if ($result = $cnxn->query($qry)) {
+        if ($result->num_rows) {
+            $table = "
+                </ul>
+                <table class='table table-striped table-responsive svbx-table'>
+                    <thead>
+                        <tr class='svbx-tr table-heading'>
+                            <th class='svbx-th id-th'>ID</th>
+                            <th class='svbx-th loc-th collapse-sm collapse-xs'>Location</th>
+                            <th class='svbx-th sev-th collapse-xs'>Severity</th>
+                            <th class='svbx-th created-th collapse-md  collapse-sm collapse-xs'>Date Created</th>
+                            <th class='svbx-th status-th'>Status</th>
+                            <th class='svbx-th system-th collapse-sm collapse-xs'>System Affected</th>
+                            <th class='svbx-th desrip-th'>Brief Description</th>";
+                    if($lvl >= 1) {
+                        $table .= "
+                            <th class='svbx-th updated-th collapse-md collapse-sm collapse-xs'>Last Updated</th>
+                            <th class='svbx-th edit-th collapse-sm collapse-xs'>Edit</th>";
+                        if($lvl === 4) {
+                            $table .= "<th class='svbx-th del-th collapse-sm collapse-xs'>Delete</th>";
+                        }
+                    } $table .= "</tr></thead><tbody>";
+                    
+                while($row = $result->fetch_array()) {
+                    $table .= "
+                        <tr class='svbx-tr'>
+                            <td class='svbx-td id-td'><a href='ViewDef.php?DefID={$row[0]}' class='class1'>{$row[0]}</a></td>
+                            <td class='svbx-td loc-td collapse-sm collapse-xs'>{$row[1]}</td>
+                            <td class='svbx-td sev-td collapse-xs'>{$row[2]}</td>
+                            <td class='svbx-td created-td collapse-md  collapse-sm collapse-xs'>{$row[3]}</td>
+                            <td class='svbx-td status-td'>{$row[4]}</td>
+                            <td class='svbx-td system-td collapse-sm collapse-xs'>{$row[5]}</td>
+                            <td class='svbx-td descrip-td'>".nl2br($row[6])."</td>";
+                    if ($lvl >= 1) {
+                       $table .= "
+                            <td class='svbx-td updated-td collapse-md  collapse-sm collapse-xs'>{$row[7]}</td>
+                            <td class='svbx-td edit-td collapse-sm collapse-xs'>
+                                <form action='UpdateDef.php' method='POST' onsubmit=''/>
+                                    <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-edit'></i></button>
+                                </form>
+                            </td>";
+                    } else $table .= "</tr>";
+                    if ($lvl === 4) {
+                        $table .= "
+                            <td class='svbx-td del-td collapse-sm collapse-xs'>
+                                <form action='DeleteDef.php' method='POST' onsubmit='' onclick='return confirm(`ARE YOU SURE? Deficiencies should not be deleted, your deletion will be logged.`)'/>
+                                    <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-times'></i></button>
+                                </form>
+                            </td></tr>";
+                    }
+                }
+            $table .= "</tbody></table>";
+        } else {
+            $table .= "<h4 class='text-secondary text-center'>No results found for your search</h4>";
+        }
+        $result->close();
+    } elseif ($cnxn->error) {
+        $table .= "<h4 class='text-danger center-content'>Error: $cnxn->error</h4>";
+    }
+    return $table;
+}
+
 if($_POST['Search'] == NULL) {
     $sql = file_get_contents("CDList.sql");
     $count = "SELECT COUNT(*) FROM CDL";
@@ -195,6 +258,7 @@ if($_POST['Search'] == NULL) {
                 T.Status,
                 Y.System, 
                 A.Description,
+                A.SpecLoc,
                 A.LastUpdated
             FROM CDL A 
             LEFT JOIN Location L 
@@ -214,83 +278,26 @@ if($_POST['Search'] == NULL) {
 <header class="container page-header">
     <h1 class="page-title">Deficiencies</h1>
     <h4 class='text-purple'>
-        <?php echo $roleLvl ?>
+        <?php
+            $roleEqls = $role === 'S';
+            echo $roleLvl.' '.$role.' '.$roleEqls;
+        ?>
     </h4>
 </header>
 <?php
     echo "<main class='container main-content'>";
     echo printInfoBox($roleLvl, 'NewDef');
-    
     echo printProjectSearchBar($link, [ method => 'POST', action => 'DisplayDefs.php' ]);
-
-    if ($result = $link->query($sql)) {
-        if ($result->num_rows) {
-        echo "
-            </ul>
-            <table class='table table-striped table-responsive svbx-table'>
-                <thead>
-                    <tr class='svbx-tr table-heading'>
-                        <th class='svbx-th id-th'>ID</th>
-                        <th class='svbx-th loc-th collapse-sm collapse-xs'>Location</th>
-                        <th class='svbx-th sev-th collapse-xs'>Severity</th>
-                        <th class='svbx-th created-th collapse-md  collapse-sm collapse-xs'>Date Created</th>
-                        <th class='svbx-th status-th'>Status</th>
-                        <th class='svbx-th system-th collapse-sm collapse-xs'>System Affected</th>
-                        <th class='svbx-th desrip-th'>Brief Description</th>";
-                if($Role == 'S' OR $Role == 'A' OR $Role == 'U') {
-                    echo "
-                        <th class='svbx-th updated-th collapse-md collapse-sm collapse-xs'>Last Updated</th>
-                        <th class='svbx-th edit-th collapse-sm collapse-xs'>Edit</th>";
-                    if($Role == 'S') {
-                        echo "<th class='svbx-th del-th collapse-sm collapse-xs'>Delete</th>";
-                    }
-                } echo "</tr></thead><tbody>";
-                
-            while($row = $result->fetch_array()) {
-                echo "
-                    <tr class='svbx-tr'>
-                        <td class='svbx-td id-td'><a href='ViewDef.php?DefID={$row[0]}' class='class1'>{$row[0]}</a></td>
-                        <td class='svbx-td loc-td collapse-sm collapse-xs'>{$row[1]}</td>
-                        <td class='svbx-td sev-td collapse-xs'>{$row[2]}</td>
-                        <td class='svbx-td created-td collapse-md  collapse-sm collapse-xs'>{$row[3]}</td>
-                        <td class='svbx-td status-td'>{$row[4]}</td>
-                        <td class='svbx-td system-td collapse-sm collapse-xs'>{$row[5]}</td>
-                        <td class='svbx-td descrip-td'>".nl2br($row[6])."</td>";
-                if ($Role == 'S' OR $Role == 'A' OR $Role == 'U') {
-                   echo "
-                        <td class='svbx-td updated-td collapse-md  collapse-sm collapse-xs'>{$row[7]}</td>
-                        <td class='svbx-td edit-td collapse-sm collapse-xs'>
-                            <form action='UpdateDef.php' method='POST' onsubmit=''/>
-                                <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-edit'></i></button>
-                            </form>
-                        </td>";
-                } else echo "</tr>";
-                if ($Role == 'S') {
-                    echo "
-                        <td class='svbx-td del-td collapse-sm collapse-xs'>
-                            <form action='DeleteDef.php' method='POST' onsubmit='' onclick='return confirm(`ARE YOU SURE? Deficiencies should not be deleted, your deletion will be logged.`)'/>
-                                <button type='submit' name='q' value='".$row[0]."'><i class='typcn typcn-times'></i></button>
-                            </form>
-                        </td></tr>";
-                }
-            }
-        echo "</tbody></table>";
-        } else {
-            echo "<h4 class='text-secondary text-center'>No results found for your search</h4>";
-        }
-        $result->close();
-    } elseif ($link->error) {
-        echo "<h4 class='text-danger center-content'>Error: $link->error</h4>";
-    }
+    echo printProjectDefsTable($link, $sql, $roleLvl);
     
     echo "</main>";
     echo "
-    <script>
-        function resetSearch(ev) {
-            ev.target.form.reset();
-            ev.target.form.submit();
-        }
-    </script>";
+        <script>
+            function resetSearch(ev) {
+                ev.target.form.reset();
+                ev.target.form.submit();
+            }
+        </script>";
                     
 mysqli_close($link);
     
