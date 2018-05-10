6778c694e5a4d9f4095aab3afe18d74c193fb067
ckb <ckingbailey@gmail.com>
2018-05-10 21:43:06 +0000

    rename img uploaded for user and timestamp

--- uploadImg.php
+++ uploadImg.php
@@ -38,28 +38,33 @@ function mimetypeCheck($mimetype) {
 if (!$_FILES['error']) {
     if ($filename = basename($_FILES['userImg']['name'])) {
         $tmpName = $_FILES['userImg']['tmp_name'];
+        // name new file for username and timestamp
+        $targetFilename = $_SESSION['Username'].'_'.time();
         $targetDir = '/img_uploads';
-        $targetPath = $_SERVER['DOCUMENT_ROOT'].$targetDir;
-        $fileDest = $targetPath.'/'.$filename;
+        $targetLocalPath = $targetDir.'/'.$targetFilename;
         if ($fileIsImg = mimetypeCheck($_FILES['userImg']['type'])) {
+            $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+            echo "<h1>{$_SESSION['Username']}, {$_SESSION['UserID']}, {$_SESSION['Role']}</h1>";
             echo "<h4 style='color: darkMagenta'><i>MIME type:</i> {$fileIsImg}, {$_FILES['userImg']['type']}</h4>";
         } else {
             echo "File did not pass MIME type check: ".$_FILES['userImg']['type'];
             exit;
-        } if ($fileIsImg = fileTypeCheck(strtolower(pathinfo($filename, PATHINFO_EXTENSION)))) {
-            echo "<h4 style='color: mediumBlue'><i>file extension:</i> $fileIsImg, ".strtolower(pathinfo($filename, PATHINFO_EXTENSION))."</h4>";
+        } if ($fileIsImg = fileTypeCheck($ext)) {
+            // append file extension to target path
+            $targetLocalPath .= '.'.$ext;
+            echo "<h4 style='color: mediumBlue'><i>file extension:</i> $fileIsImg, $ext</h4>";
         } else {
             echo "File did not pass extension type check: ".strtolower(pathinfo($filename, PATHINFO_EXTENSION));
             exit;
         } if ($uploadImgData = getimagesize($tmpName)) {
-            echo "<h4 style='color: forestGreen'><i>file info:</i> {$tmpName}, $filename, {$uploadImgData[3]}, {$uploadImgData['mime']}</h4>";
+            echo "<h4 style='color: forestGreen'><i>file info:</i> {$uploadImgData[3]}, {$uploadImgData['mime']}</h4>";
         } else {
             echo "File ".$tmpName." did not pass getimagesize test";
             exit;
-        } if ($fileIsImg = move_uploaded_file($tmpName, $fileDest)) {
+        } if ($fileIsImg = move_uploaded_file($tmpName, $_SERVER['DOCUMENT_ROOT'].$targetLocalPath)) {
             echo "
-                <h4 style='color: slateBlue'><i>File moved:</i> {$tmpName} -> {$fileDest}</h4>
-                <img src='{$targetDir}/{$filename}'>";
+                <h4 style='color: slateBlue'><i>File moved:</i> {$tmpName} -> {$_SERVER['DOCUMENT_ROOT']}{$targetLocalPath}</h4>
+                <img src='{$targetLocalPath}'>";
         } else {
             echo "Could not move file $tmpName -> $fileDest";
         }
