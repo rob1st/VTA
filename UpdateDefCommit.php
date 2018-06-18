68cf6b54ac4f87af83529027eed519a8ea9a0eef
ckb <ckingbailey@gmail.com>
2018-06-18 11:59:16 -0700

    put together insert for cdlComments, as yet untested

--- UpdateDefCommit.php
+++ UpdateDefCommit.php
@@ -3,10 +3,13 @@ session_start();
 include('SQLFunctions.php');
 include('uploadImg.php');
 
+$link = f_sqlConnect();
+
 // prepare POST and sql string for commit
 $post = $_POST;
 $defID = $post['defID'];
 $userID = $_SESSION['UserID'];
+$username = $_SESSION['Username'];
 
 // validate POST data
 // if it's empty then file upload exceeds post_max_size
@@ -16,119 +19,134 @@ if (!count($post) || !$defID) {
     exit;
 }
 
+// if photo in POST it will be committed to a separate table
+if ($_FILES['CDL_pics']['size']
+    && $_FILES['CDL_pics']['name']
+    && $_FILES['CDL_pics']['tmp_name']
+    && $_FILES['CDL_pics']['type']) {
+    $CDL_pics = $_FILES['CDL_pics'];
+} else $CDL_pics = null;
+    
 // hold onto comments separately
-$cdlCommText = $post['cdlCommText'];
+$cdlCommText = trim($post['cdlCommText']);
 
-// check for attachment and prepare Upload object
-if ($_FILES['attachment']['size']
-    && $_FILES['attachment']['name']
-    && $_FILES['attachment']['tmp_name']
-    && $_FILES['attachment']['type']) {
-    $folder = 'uploads/bartdlUploads';
-    $attachmentKey = 'attachment';
-}
-
-// unset keys from field list that will not be UPDATE'd
-$fieldList = preg_replace('/\s+/', '', file_get_contents('bartdl.sql'));
+// prepare parameterized string from external .sql file
+$fieldList = preg_replace('/\s+/', '', file_get_contents('UpdateDef.sql'));
 $fieldsArr = array_fill_keys(explode(',', $fieldList), '?');
-unset($fieldsArr['id'], $fieldsArr['created_by'], $fieldsArr['form_modified']);
 
-// append keys that do not or may not come from html form
-$post = ['updated_by' => $_SESSION['UserID']] + $post;
-$post['resolution_disputed'] || $post['resolution_disputed'] = 0;
-$post['structural'] || $post['structural'] = 0;
+// unset keys that will not be updated before imploding back to string
+unset(
+    $fieldsArr['defID'],
+    $fieldsArr['created_by'],
+    $fieldsArr['dateCreated'],
+    $fieldsArr['lastUpdated']
+);
 
 $assignmentList = implode(' = ?, ', array_keys($fieldsArr)).' = ?';
-$sql = "UPDATE BARTDL SET $assignmentList WHERE id=$defID";
+$sql = "UPDATE CDL SET $assignmentList WHERE id=$defID";
 
+// append keys that do not or may not come from html form
+// or whose values may be ambiguous in $_POST (e.g., checkboxes)
+$post += ['updated_by' => $username];
 
-if(!empty($_POST)) {
-    $link = f_sqlConnect();
-    $defID = $_POST['DefID'];
-    $SystemAffected = $_POST['SystemAffected'];
-    $LocationName = $_POST['LocationName'];
-    $SpecLoc = $link->real_escape_string($_POST['SpecLoc']);
-    $Status = $_POST['Status'];
-    $SeverityName = $_POST['SeverityName'];
-    $GroupToResolve = $_POST['GroupToResolve'];
-    $IdentifiedBy = $link->real_escape_string($_POST['IdentifiedBy']);
-    $defType = $_POST['defType'];
-    $Description = $link->real_escape_string($_POST['Description']);
-    $EvidenceType = $_POST['EviType'];
-    $EvidenceLink = $link->real_escape_string($_POST['EvidenceLink']);
-    $ActionOwner = $link->real_escape_string($_POST['ActionOwner']);
-    $OldID = $link->real_escape_string($_POST['OldID']);
-    $comments = $link->real_escape_string($_POST['comments']);
-    $Spec = $link->real_escape_string($_POST['Spec']);
-    $ClosureComments = $link->real_escape_string($_POST['ClosureComments']);
-    $RequiredBy = $_POST['RequiredBy'];
-    $contractID = $_POST['contractID'];
-    $Repo = $_POST['Repo'];
-    $Pics = $_POST['Pics'];
-    $DueDate = $_POST['DueDate'];
-    $SafetyCert = $_POST['SafetyCert'];
-    $UserID = $_SESSION['UserID'];
-    $Username = $_SESSION['Username'];
+try {
+    $success = "<div style='background-color: pink; border: 5px dashed limeGreen;>%s</div>";
+    $successFormat = "<p style='color: %s'>%s</p>";
     
-    $sql = "UPDATE CDL
-            SET  
-                 OldID = '$OldID',
-                 Location = '$LocationName',
-                 SpecLoc = '$SpecLoc',
-                 Severity = '$SeverityName',
-                 Description = '$Description',
-                 Spec = '$Spec',
-                 Status = '$Status',
-                 IdentifiedBy = '$IdentifiedBy',
-                 defType = '$defType',
-                 SystemAffected = '$SystemAffected',
-                 GroupToResolve = '$GroupToResolve',
-                 ActionOwner = '$ActionOwner',
-                 EvidenceType = '$EvidenceType',
-                 EvidenceLink = '$EvidenceLink',
-                 comments = '$comments',
-                 SafetyCert = '$SafetyCert',
-                 Requiredby = '$RequiredBy',
-                 contractID = '$contractID',
-                 Repo = '$Repo',
-                 ClosureComments = '$ClosureComments',
-                 DueDate = '$DueDate',
-                 Updated_by = '$Username',
-                 LastUpdated = NOW(),
-                 defType = {$_POST['defType']}
-            WHERE DefID = $defID;";
-
-    // if photo in POST it will be committed to a separate table
-    if ($_FILES['CDL_pics']['size']
-        && $_FILES['CDL_pics']['name']
-        && $_FILES['CDL_pics']['tmp_name']
-        && $_FILES['CDL_pics']['type']) {
-        $CDL_pics = $_FILES['CDL_pics'];
-    } else $CDL_pics = null;
+    if (!$stmt = $link->prepare($sql)) throw new mysqli_sql_exception($link->error);
     
-    if($link->query($sql)) {
-        $qs = "defID=$defID";
-        // if INSERT succesful, prepare, upload, and INSERT photo
-        if ($CDL_pics) {
-            $pathToFile = saveImgToServer($_FILES['CDL_pics'], $defID);
-            $qs .= "&$pathToFile";
-            $sql = "INSERT CDL_pics (defID, pathToFile) values (?, ?)";
-            if ($stmt = $link->prepare($sql)) {
-                if ($stmt->bind_param('is', $defID, $pathToFile)) {
-                    if (!$stmt->execute()) $pathToFile = 'execute_failed';
-                    $stmt->close();
-                } else $pathToFile = 'bind_failed';
-            } else $pathToFile = 'prepare_failed';
-        }
+    $success = sprintf($success, sprintf($successFormat, 'blue', '✓ CDL update stmt prepared') . '%s');
+    
+    $types = 'iiisiisiiisissssiissss';
+    if (!$stmt->bind_param(
+        intval($post['safetyCert']),
+        intval($post['systemAffected']),
+        intval($post['location']),
+        $link->escape_string($post['specLoc']),
+        intval($post['status']),
+        intval($post['severity']),
+        $link->escape_string($post['dueDate']),
+        intval($post['groupToResolve']),
+        intval($post['requiredBy']),
+        intval($post['contractID']),
+        $link->escape_string($post['identifiedBy']),
+        intval($post['defType']),
+        $link->escape_string($post['description']),
+        $link->escape_string($post['spec']),
+        $link->escape_string($post['actionOwner']),
+        $link->escape_string($post['oldID']),
+        intval($post['evidenceType']),
+        intval($post['repo']),
+        $link->escape_string($post['evidenceLink']),
+        $link->escape_string($post['closureComments']),
+        $link->escape_string($post['updated_by']),
+        $link->escape_string($post['dateClosed'])
+    )) throw new mysqli_sql_exception($stmt->error);
+    
+    $success= sprintf($success, sprintf($successFormat, 'forestGreen', '✓ CDL params bound') . '%s');
+    
+    if (!$stmt->execute()) throw new mysqli_sql_exception($stmt->error);
+    
+    $success = sprintf($success, sprintf($successFormat, 'dodgerBlue', '✓ CDL insert executed') . '%s');
+    
+    $stmt->close();
+    
+    $success = sprintf($success, sprintf($successFormat, 'indigo', '✓ CDL stmt closed') . '%s');
+    
+    // if INSERT succesful, prepare, upload, and INSERT photo
+    if ($CDL_pics) {
+        $sql = "INSERT CDL_pics (defID, pathToFile) values (?, ?)";
+        if (!$stmt = $link->prepare($sql)) throw new mysqli_sql_exception($link->error);
+        $success = sprintf($success, sprintf($successFormat, 'cadetBlue', '✓ cdlPics stmt prepared') . '%s');
+        if (!$stmt->bind_param('is', $defID, $pathToFile)) throw new mysqli_sql_exception($stmt->error);
+        $success = sprintf($success, sprintf($successFormat, 'cornFlower', '✓ cdlPics params bound') . '%s');
+        if (!$stmt->execute()) throw new mysqli_sql_exception($stmt->error);
+        $success = sprintf($success, sprintf($successFormat, 'aqua', '&#x2714; cdlPics insert executed') . '%s');
+        $stmt->close();
+        $success = sprintf($success, sprintf($successFormat, 'aquamarine', '&#x2714; cdlPics stmt closed') . '%s');
     } else {
-        echo "
-        <div class='container page-header'>
-        <h5>There was a problem with the database query</h5>
-        <pre>";
-        echo $link->error;
-        echo "</pre></div>";
+        $success = sprintf($success, sprintf($successFormat, 'cyan', '&#x2714; no cdlPics found') . '%s');
     }
+    
+    // if comment submitted commit it to a separate table
+    if (count($cdlCommText)) {
+        $sql = "INSERT cdlComments (defID, cdlCommText, userID) VALUES (?, ?, ?)";
+        if (!$stmt = $link->prepare($sql)) throw new mysqli_sql_exception($link->error);
+        $success = sprintf($success, sprintf($successFormat, 'darkCyan', '&#x2714; cdlComments stmt prepared') . '%s');
+        if (!$stmt->bind_param('isi',
+            intval($defID),
+            $link->escape_string($cdlCommText),
+            intval($userid))) throw new mysqli_sql_exception($stmt->error);
+        $success = sprintf($success, sprintf($successFormat, 'darkBlue', '&#x2714; cdlComments params bound') . '%s');
+        if (!$stmt->execute()) throw new mysqli_sql_exception($stmt->error);
+        $success = sprintf($success, sprintf($successFormat, 'darkTurquoise', '&#x2714; cdlComments stmt executed') . '%s');
+        $stmt->close();
+        $success = sprintf($success, sprintf($successFormat, 'deepSkyBlue', '&#x2714; cdlComments stmt closed') . '%s');
+    } else {
+        $success = sprintf($success, sprintf($successFormat, 'mediumAquamarine', '&#x2714; no cdlComments found') . '%s');
+    }
+
     $link->close();
-    header("Location: ViewDef.php?$qs");
+    
+    $success = sprintf($success, sprintf($successFormat, 'lightSteelBlue', '&#x2714; link closed') . '%s');
+    print $success;
+    
+    // header("Location: ViewDef.php?defID=$defID");
+} catch (mysqli_sql_exception $e) {
+    print "
+        <div style='margin-top: 5.5rem; text-align: center;'>
+            <p style='min-width: 7.5rem; min-height: 6rem; background-color: coral;'>{$e->getMessage()}</p>
+        </div>";
+    $stmt->close();
+    $link->close();
+    exit;
+} catch (Exception $e) {
+    print "
+        <div style='margin-top: 5.5rem; text-align: center;'>
+            <p style='min-width: 9rem; min-height: 5rem; background-color: purple;'>{$e->getMessage()}</p>
+        </div>";
+    $stmt->close();
+    $link->close();
+    exit;
 }
 ?>
