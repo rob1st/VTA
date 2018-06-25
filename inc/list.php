966b94ca1a816b11f82fdd850145bb723580e176
ckb <ckingbailey@gmail.com>
2018-06-25 15:52:52 -0700

    fix some table names and <a href=> mappings in list.php

--- inc/list.php
+++ inc/list.php
@@ -1,7 +1,19 @@
 <?php
 require_once '../inc/sqlFunctions.php';
+require_once '../inc/sqlStrings.php';
 
-$sql = file_get_contents('../sql/listCountTables.sql');
+$sql = $sqlStrings['listAll'];
+
+$tableNames = [
+    'system' => 'System',
+    'component' => 'component',
+    'deficiency type' => 'defType',
+    'contract' => 'Contract',
+    'evidence' => 'Evidence',
+    'status' => 'Status',
+    'severity' => 'Severity',
+    'test status' => 'testStatus'
+];
 
 $link = connect();
 
@@ -14,7 +26,7 @@ while ($row = $res->fetch_row()) {
     $data[] = array(
         'name' => ucfirst($row[0]),
         'count' => $row[1],
-        'href' => "/public_html/manage.php/list/{$row[0]}"
+        'href' => "/public_html/manage.php/list/{$tableNames[$row[0]]}"
     );
 }
 
--- inc/sqlStrings.php
+++ inc/sqlStrings.php
@@ -1,22 +1,22 @@
 <?php
 $sqlStrings = [
-    'list' => [
-        "SELECT 'location' AS element, COUNT(locationID) AS count FROM Location WHERE locationName <> ''
+    'listAll' => "SELECT 'location' AS element, COUNT(locationID) AS count FROM Location WHERE locationName <> ''
             UNION
             SELECT 'system', COUNT(systemID) FROM System WHERE system <> ''
             UNION
             SELECT 'component', COUNT(compID) FROM component WHERE compName <> ''
             UNION
-            SELECT 'deficiency', COUNT(deftypeID) FROM defType WHERE deftypeName <> ''
+            SELECT 'deficiency type', COUNT(deftypeID) FROM defType WHERE deftypeName <> ''
             UNION
             SELECT 'contract', COUNT(contractID) FROM Contract WHERE contract <> ''
             UNION
             SELECT 'evidence', COUNT(eviTypeID) FROM EvidenceType WHERE eviType <> ''
             UNION
-            SELECT 'status', COUNT(testStatID) FROM testStatus WHERE testStatName <> ''
+            SELECT 'status', COUNT(statusID) FROM Status WHERE Status <> ''
             UNION
-            SELECT 'severity', COUNT(severityID) FROM Severity WHERE severityName <> ''"
-    ],
+            SELECT 'severity', COUNT(severityID) FROM Severity WHERE severityName <> ''
+            UNION
+            SELECT 'test status', COUNT(testStatID) FROM testStatus WHERE testStatName <> ''",
     'component' => [
         'types' => 'ss',
         'insert' => "INSERT component (compName, compDescrip) VALUES (?, ?)",
