b088faaf4faa3858372e7f23c13f301b62a2e810
ckb <ckingbailey@gmail.com>
2018-06-26 12:16:43 -0700

    fix component.php to work with new db handler

--- inc/component.php
+++ inc/component.php
@@ -11,32 +11,25 @@ $formCtrls = array(
 
 if ($action === 'list') {
     $sql = $sqlStrings['component']['list'];
-    
-    $link = connect();
-    
-    $res = $link->query($sql);
-    
-    $data = array();
-    
-    while ($row = $res->fetch_row()) {
-        $data[] = array(
-            'id' => $row[0],
-            'name' => $row[1],
-            'description' => $row[2],
-            'href' => "/public_html/manage.php/update/component?id={$row[0]}"
-        );
+        
+    try {
+        $link = connect();
+        
+        $data = $link->query($sql);
+        
+        foreach ($data as &$row) {
+            $row['href'] = "/public_html/manage.php/update/component?id={$row[0]}";
+        }
+        
+        $count = $link->count;
+    } catch (mysqli_sql_exception $e) {
+        echo "<pre style='color: deepPink'>There was a problem fetching from the database: $e</pre>";
+    } finally {
+        $link->disconnect();
+        $cardHeading = $action === 'list'
+            ? 'Components'
+            : 'Enter component information';
+        $tableName = 'component';
+        $target = 'commitNewData.php';
     }
-    
-    $count = $res->num_rows;
-    
-    $res->close();
-    $link->close();
 }
-
-$title = ucwords($action . ' ' . $table);
-$pageHeading = ucfirst($action . ' ' . $table);
-$cardHeading = $action === 'list'
-    ? 'Components'
-    : 'Enter component information';
-$tableName = 'component';
-$target = 'commitNewData.php';
