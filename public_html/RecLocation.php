ba8aaf8dcdd901d279278f8e6d98403d169a2eab
ckb <ckingbailey@gmail.com>
2018-07-19 11:13:03 -0700

    fix table name and var names in RecLocation

--- public_html/RecLocation.php
+++ public_html/RecLocation.php
@@ -1,55 +1,40 @@
 <?PHP
     include('sql_functions/sqlFunctions.php');
-    session_Start();
-    $table = location;
-    
-
-    echo '<br>display full contents of the _POST: <br>';
-    var_dump($_POST);
+    session_start();
+    $table = 'location';
     
     $link = f_sqlConnect();
     $check = "SELECT * FROM $table WHERE LocationName = '".$_POST['LocationName']."'";
     $UserID = $_SESSION['userID'];
-    $user = "SELECT Username FROM users_enc WHERE UserID = ".$UserID;
-    if($result=mysqli_query($link,$user)) 
-        {
-          /*from the sql results, assign the username that returned to the $username variable*/    
-          while($row = mysqli_fetch_assoc($result)) {
-            $Username = $row['username'];
-          }
-        }
+    $user = "SELECT username FROM users_enc WHERE UserID = ".$UserID;
+    if($result=mysqli_query($link,$user)) {
+      /*from the sql results, assign the username that returned to the $username variable*/    
+      while($row = mysqli_fetch_assoc($result)) {
+        $Username = $row['username'];
+      }
+    }
     $keys = implode(", ", (array_keys($_POST)));
-    echo '<br>Parsed Key: ' .$keys;
     $values = implode("', '", (array_values($_POST)));
-    echo '<br>Parsed Values: ' .$values;
     
-    if(!f_tableExists($link, $table, DB_Name)) {
-        die('<br>Destination table does not exist:'.$table);
-    }
+    // if(!f_tableExists($link, $table, DB_Name)) {
+    //     die('<br>Destination table does not exist:'.$table);
+    // }
     
-    $result = mysqli_query($link,$check);
-    $num_rows = mysqli_num_rows($result);
+    // $result = mysqli_query($link,$check);
+    // $num_rows = mysqli_num_rows($result);
 
-    if ($num_rows > 0) {
-      header("location: $duplicate?msg=1");
-    }
-    else {
+    // if ($num_rows > 0) {
+    //   header("location: $duplicate?msg=1");
+    // }
+    // else {
     $sql = "INSERT INTO $table($keys, lastUpdated, updatedBy) VALUES ('$values', NOW(), '$Username')";
-    //echo '<br>sql: ' .$sql;
-    //echo '<br>Num_rows: ' .$num_rows;
     
     if (!mysqli_query($link,$sql)) {
-		echo '<br>Error: ' .mysqli_error($link);
-		if(!empty($rejectredirecturl)) {
-	    	//header("location: $rejectredirecturl?msg=1");
-	    	echo $sql;
-    }    
-	
-    }else if(!empty ($rejectredirecturl)) {
-            header("location: DisplayLocations.php");
-            //echo "Success";
+	  	echo '<br>Error: ' .mysqli_error($link);
+    } else {
+      header("location: DisplayLocations.php");
     }
-}
-    
+// }
 	mysqli_close($link);
+	exit;
 ?>
