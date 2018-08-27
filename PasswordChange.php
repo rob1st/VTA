3f38699c2277c8a17216122873919c726cf26ee8
ckb <ckingbailey@gmail.com>
2018-08-27 10:08:10 -0700

    use $_SESSION[userid] to grab user detail in pass change

--- PasswordChange.php
+++ PasswordChange.php
@@ -2,11 +2,11 @@
 
 include('session.php');
 $Username = $_POST['username'];
-$oldpw = ($_POST['oldpw']);
-$newpw = ($_POST['newpw']);
+$oldpwd = filter_var($_POST['oldpw'], FILTER_SANITIZE_STRING);
+$newpwd = filter_var($_POST['newpw'], FILTER_SANITIZE_STRING);
 
     
-if(empty($oldpw))
+if(empty($oldpwd))
 {
     $message = 'Please enter your old password';
 }
@@ -24,8 +24,6 @@ elseif ($_POST['newpw'] <> $_POST['conpw'])
 }
 elseif(!empty($_POST)) {
     $UserID = $_POST['userID'];
-    $oldpwd = filter_var($_POST['oldpw'], FILTER_SANITIZE_STRING);
-    $newpwd = filter_var($_POST['newpw'], FILTER_SANITIZE_STRING);
     
     try {
         
@@ -33,7 +31,7 @@ elseif(!empty($_POST)) {
         $link = f_sqlConnect();
         
         // check whether username exists
-        $query = "SELECT Password FROM users_enc WHERE userID='$userID'";
+        $query = "SELECT Password FROM users_enc WHERE userID='{$_SESSION['userID']}'";
         if($result=mysqli_query($link,$query)) {
         while($row = mysqli_fetch_assoc($result)) {
             $get_password = $row['Password'];
@@ -45,30 +43,30 @@ elseif(!empty($_POST)) {
         // check that old password is correct
         $auth = password_verify($oldpwd, $get_password);
             
-        if($auth == 1) {
-        $new_pwd = password_hash($newpwd, PASSWORD_BCRYPT);
+        if ($auth == 1) {
+            $new_pwd = password_hash($newpwd, PASSWORD_BCRYPT);
         
-                $query = "
-                        UPDATE 
-                            users_enc 
-                        SET 
-                            Password = '$new_pwd'
-                            ,updated_By = '$Username'
-                            ,LastUpdated = NOW()
-                        WHERE 
-                            Username = '$Username'";
-                            
-              mysqli_query($link, $query) or
-                    die("Insert failed. " . mysqli_error($link));
-              
-              $message = "<p class='message'>Your password has been changed</p>";
-              
-              mysqli_free_result($result);
-            } else {
-               // Username or password is incorrect
-                $message = "<p class='message'>Error: Your username and password do not match.</p>" ;
-            } 
-        } catch(Exception $e) { $message = "Unable to process request";
+            $query = "
+                    UPDATE 
+                        users_enc 
+                    SET 
+                        Password = '$new_pwd'
+                        ,updated_By = '$Username'
+                        ,LastUpdated = NOW()
+                    WHERE 
+                        Username = '$Username'";
+                        
+          mysqli_query($link, $query) or
+                die("Insert failed. " . mysqli_error($link));
+          
+          $message = "<p class='message'>Your password has been changed</p>";
+          
+          mysqli_free_result($result);
+        } else {
+           // Username or password is incorrect
+            $message = "<p class='message'>Error: Your username and password do not match.</p>" ;
+        } 
+    } catch(Exception $e) { $message = "Unable to process request";
 }
 }
 ?>
